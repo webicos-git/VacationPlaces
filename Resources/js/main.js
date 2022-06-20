0315c8e1cc511d0e09eb3f471996269a4d670b10
Ashraf-Sonde <ashrafsonde@gmail.com>
2022-06-20 11:43:30 +0530

    minor changes in UI and filters

--- Resources/js/main.js
+++ Resources/js/main.js
@@ -1,50 +1,58 @@
 
 $("#five").change(function() {
+    $("#hotel_count").css("opacity", "0");
+
     if(this.checked) {
-    $("#id4").css("display", "none");
-    $("#id3").css("display", "none");
-    $("#id2").css("display", "none");
+    $(".id4").css("display", "none");
+    $(".id3").css("display", "none");
+    $(".id2").css("display", "none");
     }else{
-        $("#id4").css("display", "block");
-        $("#id3").css("display", "block");
-        $("#id2").css("display", "block");
+        $(".id4").css("display", "block");
+        $(".id3").css("display", "block");
+        $(".id2").css("display", "block");
     }
 });
 
 $("#four").change(function() {
+    $("#hotel_count").css("opacity", "0");
+
     if(this.checked) {
-    $("#id5").css("display", "none");
-    $("#id3").css("display", "none");
-    $("#id2").css("display", "none");
+    $(".id5").css("display", "none");
+    $(".id3").css("display", "none");
+    $(".id2").css("display", "none");
     }else{
-        $("#id5").css("display", "block");
-        $("#id3").css("display", "block");
-        $("#id2").css("display", "block");
+        $(".id5").css("display", "block");
+        $(".id3").css("display", "block");
+        $(".id2").css("display", "block");
     }
 });
 
 
 $("#three").change(function() {
+    $("#hotel_count").css("opacity", "0");
+
     if(this.checked) {
-    $("#id4").css("display", "none");
-    $("#id5").css("display", "none");
-    $("#id2").css("display", "none");
+    $(".id4").css("display", "none");
+    $(".id5").css("display", "none");
+    $(".id2").css("display", "none");
     }else{
-        $("#id4").css("display", "block");
-        $("#id5").css("display", "block");
-        $("#id2").css("display", "block");
+        $(".id4").css("display", "block");
+        $(".id5").css("display", "block");
+        $(".id2").css("display", "block");
     }
 });
 
 $("#two").change(function() {
+    $("#hotel_count").css("opacity", "0");
+
     if(this.checked) {
-    $("#id4").css("display", "none");
-    $("#id5").css("display", "none");
-    $("#id3").css("display", "none");
+    $(".id4").css("display", "none");
+    $(".id5").css("display", "none");
+    $(".id3").css("display", "none");
     }else{
-        $("#id4").css("display", "block");
-        $("#id5").css("display", "block");
-        $("#id3").css("display", "block");
+        $(".id4").css("display", "block");
+        $(".id5").css("display", "block");
+        $(".id3").css("display", "block");
     }
 });
 
@@ -54,7 +62,4 @@ $('#range').change(function(){
     $("#rangeSliderExample3MaxResult").text(max);
     let minAmount=$("#rangeSliderExample3MinResult").text();
     let maxAmount=$("#rangeSliderExample3MaxResult").text();
-    
-
-
-    });
+});
